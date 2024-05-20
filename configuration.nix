edc20c8982272a354cae506a5db411ad6d83d346
0Supa <supa.codes@gmail.com>
2024-05-20 17:37:47 +0300

    + ipv6 dns

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,12 @@
 
   networking = {
     hostName = "Kappa";
-    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
     networkmanager.enable = true;
   };
 
