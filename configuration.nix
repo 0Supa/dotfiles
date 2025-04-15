81297800af3fb9f0da9298b335be28c0ecb1b752
Supa <36031171+0Supa@users.noreply.github.com>
2025-04-15 09:44:52 +0300

    technorino: add overlay

--- configuration.nix
+++ configuration.nix
@@ -157,7 +157,7 @@
           librewolf
           chromium
           firefox
-          chatterino7
+          technorino
           pkgs-stable.electrum # BTC wallet
           # monero-gui # XMR wallet
           qbittorrent
@@ -344,6 +344,27 @@
 
       (final: prev: {
         foot = pkgs-stable.foot;
+      })
+
+      (final: prev: {
+        technorino = prev.chatterino7.overrideAttrs (oldAttrs: {
+          pname = "technorino";
+          version = "nightly";
+
+          buildInputs =
+            oldAttrs.buildInputs
+            ++ (with prev; [
+              kdePackages.qtimageformats
+              libnotify
+            ]);
+          src = final.chatterino7.src.override {
+            owner = "2547techno";
+            repo = "technorino";
+            tag = null;
+            rev = "ca72ecfcc8db40c16051adf7c426a01eaffd5c6c";
+            hash = "sha256-pG96nAyjEtfeOCkhG5rTs1g3Vq4lXHpLSDq0Tr196wQ=";
+          };
+        });
       })
     ];
   };
